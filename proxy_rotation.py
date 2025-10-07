5f684511d945e1dab9ca6f82c1eac7b34752287e
Urten <n363363@gmail.com>
2025-10-07 22:49:38 +0530

    fixed in script proxy_rotation.py

--- proxy_rotation.py
+++ proxy_rotation.py
@@ -13,12 +13,13 @@ def proxy_test(proxy):
     try:
 
         protocol = proxy.split("://")[0]
+        country = proxy.split("#")[1]
         proxies = {protocol: proxy}
         response = requests.get("http://httpbin.org/ip", proxies=proxies, timeout=5)
 
         if response.status_code == 200:
             print(f"Proxy {proxy} is working.")
-            working_proxies.append(proxy)
+            working_proxies.append((proxy, country))
         else:
             print(f"Proxy {proxy} returned status code {response.status_code}.")
     except Exception as e:
@@ -30,12 +31,17 @@ for item in proxies_json.get("data", []):
     ip = item.get("ip")
     port = item.get("port")
     protocol = item.get("protocols")[0]
+    country = item.get("country")
     if ip and port:
-        proxy_list.append(f"{protocol}://{ip}:{port}")
+        proxy_list.append(f"{protocol}://{ip}:{port}#{country}")
 
 # Test proxies in parallel
-with ThreadPoolExecutor(max_workers=100) as executor:
-    executor.map(proxy_test, proxy_list)
 
 
-print(f"Working proxies: {working_proxies}")
+def get_working_proxies():
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        executor.map(proxy_test, proxy_list)
+        
+    return working_proxies
+
+print(get_working_proxies())
